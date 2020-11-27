9e03afebfb5151872a2103887dc17026144138b4
AkitoHamamoto <akatsuki.blog.fun@gmail.com>
2020-11-27 15:42:08 +0900

    新規登録実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :room_users
+  # has_many :rooms, through: room_users
+  has_many :messages
+  has_many :posts
+
+  with_options presence: true do
+    validates :nickname
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX, message: 'には@のつくアドレスを入力してください' }
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, length: { minimum: 6 }, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+    validates :birth_date
+    validates :authority
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/index'
   root to: 'users#index'
-  resources :posts, only:[:index]
+  resources :users, only:[:index, :edit, :show]
+  resources :posts, only:[:index, :new, :show, :edit]
+  resources :rooms, only:[:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 
 end
