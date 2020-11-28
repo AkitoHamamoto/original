81e82d436b0312e9be0a16382ab649c1ff6e7b67
AkitoHamamoto <akatsuki.blog.fun@gmail.com>
2020-11-28 13:25:52 +0900

    フロント実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,5 @@ class User < ApplicationRecord
     validates :birth_date
     validates :authority
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/index'
   root to: 'users#index'
-  resources :users, only:[:index, :edit, :show]
+  resources :users, only:[:index, :edit, :show, :update]
   resources :posts, only:[:index, :new, :show, :edit]
   resources :rooms, only:[:new, :create] do
     resources :messages, only: [:index, :create]
