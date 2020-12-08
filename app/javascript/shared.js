31f0ceeb651ac0a66ca9ed23a0d85c79558893b0
AkitoHamamoto <akatsuki.blog.fun@gmail.com>
2020-12-08 19:25:02 +0900

    ユーザー詳細ページ実装

--- app/javascript/shared.js
+++ app/javascript/shared.js
@@ -5,11 +5,13 @@ function menu (){
 
   pullDownButton.addEventListener('mouseover', function(){
     menu.setAttribute("style","display: block" )
+    // menu.animate({opacity: '0'}, {opacity: '1'}, 1500)
   })
 
   pullDownButton.addEventListener('mouseout', function(){
     menu.setAttribute("style","display: none" )
   })
+
 }
 
 setInterval(menu, 1000)
